372ec7856bb8c37431f83ddff71f60561c3f99b0
Gerasimos01 <acivil@hotmail.gr>
2018-05-14 19:56:43 +0300

    geometry modifier for rotation and translation

--- ISAAR.MSolve.SamplesConsole/RVEkanoninkhsGewmetriasBuilder.cs
+++ ISAAR.MSolve.SamplesConsole/RVEkanoninkhsGewmetriasBuilder.cs
@@ -440,7 +440,7 @@ namespace ISAAR.MSolve.SamplesConsole
             var embeddedGrouping = new EmbeddedCohesiveGrouping_v2(model, hostGroup, embdeddedGroup);
         }
 
-        public static void CreateRandomDataForGeom(int n_graphene_sheets, grapheneSheetParameters gp, rveMatrixParameters mp,double sigma_f)
+        public static Tuple<double[], double[], double[][]> CreateRandomDataForGeom(int n_graphene_sheets, grapheneSheetParameters gp, rveMatrixParameters mp,double sigma_f)
         {
             //PROELEFSI REFERENCE_kanonikh_gewmetria\REF2_50_000_renu_new_multiple_algorithms_check_develop
             // 'ekk_xyz','phi1_ij','phi2_ij','rot_phi_1','rot_phi_2'
@@ -593,8 +593,60 @@ namespace ISAAR.MSolve.SamplesConsole
                 }
             }
 
-            // return 
+            
+            //Tuple<double[], double[], double[][]> randomDataForRotationAndTranslation = new Tuple<double[], double[], double[][]>(rot_phi_1, rot_phi_2, ekk_xyz);
+            return new Tuple<double[], double[], double[][]>(rot_phi_1, rot_phi_2, ekk_xyz);
 
+        }
+
+        public static double[] modify_ox_sunol_forRotationAndTranslation(double[] o_xsunol, double rot_phi_1, double rot_phi_2, double[] ekk_xyz)
+        {
+            
+            // rotation data
+            double e1_new_z = Math.Sin(rot_phi_2);
+            double e1_new_y = Math.Sin(rot_phi_1) * Math.Cos(rot_phi_2); // e1_new_xy = cos(rot_phi_2);
+            double e1_new_x = Math.Cos(rot_phi_1) * Math.Cos(rot_phi_2);
+
+            double e2_new_y = Math.Cos(rot_phi_1);
+            double e2_new_x = -Math.Sin(rot_phi_1);
+            double e2_new_z = 0;
+
+            double[,] e_new = new double[3, 3] { { e1_new_x, e2_new_x, 0 }, { e1_new_y, e2_new_y, 0 }, { e1_new_z, e2_new_z, 0 } };
+            double[] e_new_cross = new double[3];
+            RVEExamplesBuilder.cross(new double[3] { e1_new_x, e1_new_y, e1_new_z }, new double[3] { e2_new_x, e2_new_y, e2_new_z }, e_new_cross);
+            for (int i1 = 0; i1 < 3; i1++)
+            { e_new[i1, 2] = e_new_cross[i1]; }
+
+            double[,] e_old = new double[3, 3] { { 1, 0, 0 }, { 0, 1, 0 }, { 0, 0, 1 } };
+
+            double[,] Qij = new double[3, 3];
+            for (int q1 = 0; q1 < 3; q1++)
+            {
+                for (int q2 = 0; q2 < 3; q2++)
+                {
+                    Qij[q1, q2] = RVEExamplesBuilder.dot_product(new double[3] { e_old[0, q1], e_old[1, q1], e_old[2, q1] }, new double[3] { e_new[0, q2], e_new[1, q2], e_new[2, q2] });
+                }
+            }
+
+            // dhmiourgia neou dianusmatos me ROTATION tou initial
+            double[] o_xsunol_new = new double[o_xsunol.GetLength(0)];
+
+            for (int komvos = 0; komvos < o_xsunol.GetLength(0)/6; komvos++)
+            {
+                double[] product;
+                product = RVEExamplesBuilder.MatVecMult(Qij, new double[3] { o_xsunol[6*(komvos)+0], o_xsunol[6 * (komvos) + 1], o_xsunol[6 * (komvos) + 2] });
+                for (int q2 = 0; q2 < 3; q2++) { o_xsunol_new[6 * (komvos) + q2] = product[q2]; }
+                product = RVEExamplesBuilder.MatVecMult(Qij, new double[3] { o_xsunol[6 * (komvos) + 3], o_xsunol[6 * (komvos) + 4], o_xsunol[6 * (komvos) + 5] });
+                for (int q2 = 0; q2 < 3; q2++) { o_xsunol_new[6 * (komvos) + 3 + q2] = product[q2]; }
+            }
+
+            // TRANSLATION dlh h ekkentrothta
+            for (int komvos = 0; komvos < o_xsunol.GetLength(0) / 6; komvos++)
+            {
+                for (int q2 = 0; q2 < 3; q2++) { o_xsunol_new[6 * (komvos) + q2] = o_xsunol_new[6 * (komvos) + q2]+ekk_xyz[q2]; }
+            }
+
+            return o_xsunol_new;
         }
 
         public static void createGrShBoxSurroundingPlanes(double L1,double L2,double sigma_f,double rot_phi_1,double rot_phi_2,double [] ekk_xyz,double[,] line_points, double[,] line_segments, double[,] pl_points, double[,] vec1s, double[,] vec2s, double[,]perp_vec3s)
