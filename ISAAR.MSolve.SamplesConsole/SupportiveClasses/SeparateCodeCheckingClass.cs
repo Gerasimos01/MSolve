e0ff4399094fbe233560c52ea117929469b1e457
Gerasimos01 <acivil@hotmail.gr>
2018-06-29 13:40:28 +0300

    minor changes Random Orientations separate class

---
+++ ISAAR.MSolve.SamplesConsole/SupportiveClasses/SeparateCodeCheckingClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ISAAR.MSolve.SamplesConsole.SupportiveClasses
+{
+    class SeparateCodeCheckingClass
+    {
+        private static void check01()
+        {
+
+        }
+    }
+}
