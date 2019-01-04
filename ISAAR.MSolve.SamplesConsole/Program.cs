f34bf610e85b590073215b0e40ade7e00bfe3c24
Gerasimos01 <acivil@hotmail.gr>
2019-01-04 21:03:51 +0200

    commit to check branches

--- ISAAR.MSolve.SamplesConsole/Program.cs
+++ ISAAR.MSolve.SamplesConsole/Program.cs
@@ -163,11 +163,11 @@ namespace ISAAR.MSolve.SamplesConsole
             //SeparateCodeCheckingClass.Check05fStressIntegration();
             //SeparateCodeCheckingClass.Check05gStressIntegration();
             //ProgramElegxoiDdm.SolveRVEExample(); //provolos hexa grammika apla
-            SeparateCodeCheckingClass.Check13RunFe2Shell();
+            //SeparateCodeCheckingClass.Check13RunFe2Shell();
             //SeparateCodeCheckingClass.Check14b();
             //SeparateCodeCheckingClass.Check05hStressIntegration();
             //SeparateCodeCheckingClass.Check05h2StressIntegration();
-            //SeparateCodeCheckingClass.Check05h3StressIntegration();
+            SeparateCodeCheckingClass.Check05h3StressIntegration();
             //SeparateCodeCheckingClass.Check05h3ConstiOnlyStressIntegration();
         }
     }
