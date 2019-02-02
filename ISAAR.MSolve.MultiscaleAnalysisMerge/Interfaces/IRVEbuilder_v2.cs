b6eb09a267a56ab62b7be2b8db51a43419123d0c
Gerasimos01 <acivil@hotmail.gr>
2019-02-02 17:38:58 +0200

    added new rve builders to update to v2

---
+++ ISAAR.MSolve.MultiscaleAnalysisMerge/Interfaces/IRVEbuilder_v2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ISAAR.MSolve.FEM.Entities;
+
+
+namespace ISAAR.MSolve.MultiscaleAnalysis.Interfaces
+{
+    public interface IRVEbuilder_v2
+    {
+        Tuple<Model_v2, Dictionary<int, Node>,double> GetModelAndBoundaryNodes();
+        IRVEbuilder_v2 Clone(int a);
+    }
+}
---
+++ ISAAR.MSolve.MultiscaleAnalysisMerge/Interfaces/IdegenerateRVEbuilder_v2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ISAAR.MSolve.Discretization.Interfaces;
+using ISAAR.MSolve.FEM.Entities;
+
+
+namespace ISAAR.MSolve.MultiscaleAnalysis.Interfaces
+{
+    public interface IdegenerateRVEbuilder_v2 : IRVEbuilder_v2
+    {
+        Dictionary<Node, IList<DOFType>> GetModelRigidBodyNodeConstraints(Model_v2 model);
+    }
+}
