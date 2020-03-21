b7a56afe82e1d064f527ba460e584a7925b4d90c
Gerasimos <acivil@hotmail.gr>
2020-03-21 12:13:11 +0200

    added test and rve builders.

---
+++ ISAAR.MSolve.MSAnalysis/RveTemplatesPaper/CntReinforcedElasticNanocompositeEmbedding.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using ISAAR.MSolve.Discretization;
+using ISAAR.MSolve.Discretization.FreedomDegrees;
+using ISAAR.MSolve.Discretization.Integration.Quadratures;
+using ISAAR.MSolve.Discretization.Interfaces;
+using ISAAR.MSolve.Discretization.Mesh;
+using ISAAR.MSolve.FEM.Elements;
+using ISAAR.MSolve.FEM.Elements.SupportiveClasses;
+using ISAAR.MSolve.FEM.Embedding;
+using ISAAR.MSolve.FEM.Entities;
+using ISAAR.MSolve.FEM.Postprocessing;
+using ISAAR.MSolve.LinearAlgebra.Matrices;
+using ISAAR.MSolve.LinearAlgebra.Vectors;
+using ISAAR.MSolve.Materials;
+using ISAAR.MSolve.Materials.Interfaces;
+using ISAAR.MSolve.MultiscaleAnalysis.Interfaces;
+using ISAAR.MSolve.Solvers.DomainDecomposition.Dual.FetiDP.CornerNodes;
+using Troschuetz.Random;
+
+namespace ISAAR.MSolve.MSAnalysis.RveTemplatesPaper
+{
+    public class CntReinforcedElasticNanocompositeEmbedding : IdegenerateRVEbuilder
+    {
+        int hexa1 = 10;
+        int hexa2 = 10;
+        int hexa3 = 10;
+
+        double L01 = 100;
+        double L02 = 100;
+        double L03 = 100;
+        IIsotropicContinuumMaterial3D matrixMaterial;
+
+
+        // cnt paramaters
+        IIsotropicContinuumMaterial3D CntMaterial;
+        int numberOfCnts;
+        // define mechanical properties
+        private double youngModulus = 1.0;//1.051e12; // 5490; // 
+        private double shearModulus = 1.0;//0.45e12; // 871; // 
+        double poissonRatio;  //2.15; // 0.034;
+        double area = 694.77; // 1776.65;  // CNT(20,20)-LinearEBE-TBT-L = 10nm
+        double inertiaY = 100.18; //1058.55;
+        double inertiaZ = 100.18; //1058.55;1058.55;
+        double torsionalInertia = 68.77; //496.38;
+        double effectiveAreaY; 
+        double effectiveAreaZ;
+        int subdomainID = 0;
+
+        public CntReinforcedElasticNanocompositeEmbedding(IIsotropicContinuumMaterial3D matrixMaterial)
+        {
+            this.matrixMaterial = matrixMaterial;
+
+            this.CntMaterial = new ElasticMaterial3D()
+            {
+                YoungModulus = youngModulus,
+                PoissonRatio = (youngModulus / (2 * shearModulus)) - 1
+            };
+
+            double effectiveAreaY = area;
+            double effectiveAreaZ = area;
+
+            
+        }
+
+        public Tuple<Model, Dictionary<int, Node>, double> GetModelAndBoundaryNodes()
+        {
+            (int[] NodeIds, double[,] node_coords) = GetHexaRveNodesData();
+            int [,] elementConnectivity= GetHexaRveConnectivity();
+
+            Model model= new Model();
+            model.SubdomainsDictionary[0] = new Subdomain(0);
+            AddHexaElements(model,NodeIds,node_coords, elementConnectivity);
+            var hostElements = model.Elements.Count()-1;
+            var hostNodes = model.Nodes.Count();
+
+
+            (int[] cntNodeIds, double[,] cntNodecoords, int[,] cntElementConnectivity) = GetCntBeamsNodesData(hostNodes, hostElements);
+            AddCntBeamElements(model, cntNodeIds, cntNodecoords, cntElementConnectivity);
+            
+            var embeddedGrouping = EmbeddedBeam3DGrouping.CreateFullyBonded(model, model.ElementsDictionary
+                        .Where(x => x.Key <= hostElements).Select(kv => kv.Value), model.ElementsDictionary.Where(x => x.Key > hostElements)
+                        .Select(kv => kv.Value), true);
+
+            var paraviewEmbedded =
+                new ParaviewEmbedded3D(model, null, Path.Combine(Directory.GetCurrentDirectory(), "ParaviewCNT"));
+            paraviewEmbedded.CreateParaviewFile();
+
+            var boundaryNodesIds = GetBoundaryNodeIds();
+            boundaryNodesIds.Sort();
+            Dictionary<int, Node> boundaryNodes = boundaryNodesIds.ToDictionary(t => t, t => model.NodesDictionary[t]);
+            return new Tuple<Model, Dictionary<int, Node>, double>(model, boundaryNodes, L01 * L02 * L03);
+        }
+
+        private List<int> GetBoundaryNodeIds()
+        {
+            var boundaryNodes= new List<int>();
+
+            for (int k = 0; k < hexa3+1; k++)
+            {
+                var indexZeta = (hexa1 + 1) * (hexa2 + 1) * k;
+                for (int i = 0; i < hexa1+1; i++)
+                {
+                    boundaryNodes.Add(i + indexZeta);
+                    boundaryNodes.Add(i +(hexa1+1)*hexa2+ indexZeta);
+                }
+
+                for (int j = 0; j < hexa2+1; j++)
+                {
+                    boundaryNodes.Add(indexZeta + (hexa1 + 1) * j);
+                    boundaryNodes.Add(indexZeta + (hexa1 + 1) * j + hexa1);
+                }
+            }
+
+            return boundaryNodes.Distinct().ToList();
+        }
+
+        private void AddCntBeamElements(Model model, int[] cntNodeIds, double[,] cntNodeCoordinates, int[,] cntElementConnectivity)
+        {
+            var hostElements = model.Elements.Count;
+            for (int i = 0; i < cntNodeIds.Length; i++)
+            {
+                var nodeID = cntNodeIds[i];
+                var coordX = cntNodeCoordinates[i, 0];
+                var coordY = cntNodeCoordinates[i, 1];
+                var coordZ = cntNodeCoordinates[i, 2];
+
+                model.NodesDictionary.Add(nodeID, new Node(id: nodeID, x: coordX, y: coordY, z: coordZ));
+            }
+            
+            var beamSection =
+                new BeamSection3D(area, inertiaY, inertiaZ, torsionalInertia, effectiveAreaY, effectiveAreaZ);
+
+            for (int i = 0; i < cntElementConnectivity.GetLength(0); i++)
+            {
+                var elementNodes = new List<Node>
+                {
+                    model.NodesDictionary[cntElementConnectivity[i, 0]],
+                    model.NodesDictionary[cntElementConnectivity[i, 1]]
+                };
+
+                var beam_1 = new Beam3DCorotationalQuaternion(elementNodes, CntMaterial, 7.85, beamSection);
+                var beamElement = new Element { ID = i+hostElements, ElementType = beam_1 };
+
+                beamElement.AddNode(elementNodes[0]);
+                beamElement.AddNode(elementNodes[1]);
+
+                model.ElementsDictionary.Add(beamElement.ID, beamElement);
+                model.SubdomainsDictionary[0].Elements.Add(beamElement);
+            }
+            
+        }
+
+        private (int[] cntNodeIds, double[,] cntNodeCoordinates, int[,] cntElementConnectivity) GetCntBeamsNodesData(int hostNodes, int hostElements)
+        {
+            int numberOfCnts = 2;
+            var cntNodeIds = new int[numberOfCnts*2];
+            var cntNodeCoordinates = new double[numberOfCnts * 2, 3];
+            var cntElementConnectivity = new int[numberOfCnts, 2];
+
+            var trandom = new TRandom();
+            for (int i1 = 0; i1 < numberOfCnts; i1++)
+            {
+                var randomY = trandom.ContinuousUniform(-L02/2.0, L02/2.0);
+                var randomZ = trandom.ContinuousUniform(-L03/2.0, L03/2.0);
+
+                cntNodeIds[2 * i1] = hostNodes + 2 * i1;
+                cntNodeIds[2 * i1 + 1] = hostNodes + 2 * i1 + 1;
+
+                
+
+                cntElementConnectivity[i1, 0] = cntNodeIds[2 * i1];
+                cntElementConnectivity[i1, 1] = cntNodeIds[2 * i1 + 1];
+            }
+
+            int i = 0;
+            cntNodeCoordinates[2 * i, 0] = -L01 / 2.0 + 1;
+            cntNodeCoordinates[2 * i, 1] = -L02 / 2.0;
+            cntNodeCoordinates[2 * i, 2] = L02 / 2.0;
+
+            cntNodeCoordinates[2 * i + 1, 0] = L01 / 2.0 - 1;
+            cntNodeCoordinates[2 * i + 1, 1] = -L02 / 2.0;
+            cntNodeCoordinates[2 * i + 1, 2] = L02 / 2.0;
+
+            i = 1;
+            cntNodeCoordinates[2 * i, 0] = -L01 / 2.0 + 1;
+            cntNodeCoordinates[2 * i, 1] = +L02 / 2.0;
+            cntNodeCoordinates[2 * i, 2] = +L02 / 2.0;
+
+            cntNodeCoordinates[2 * i + 1, 0] = L01 / 2.0 - 1;
+            cntNodeCoordinates[2 * i + 1, 1] = +L02 / 2.0;
+            cntNodeCoordinates[2 * i + 1, 2] = +-L02 / 2.0;
+
+
+            return (cntNodeIds, cntNodeCoordinates, cntElementConnectivity);
+        }
+
+        private void AddHexaElements(Model model, int[] nodeIds, double[,] node_coords, int[,] elementConnectivity)
+        {
+            for (int i1 = 0; i1 < nodeIds.Length; i1++)
+            {
+                int nodeID = nodeIds[i1];
+                double nodeCoordX = node_coords[i1, 0];
+                double nodeCoordY = node_coords[i1, 1];
+                double nodeCoordZ = node_coords[i1, 2];
+
+                model.NodesDictionary.Add(nodeID, new Node(id: nodeID, x: nodeCoordX, y: nodeCoordY, z: nodeCoordZ));
+            }
+            
+            //var factoryOutter = new ContinuumElement3DFactory(matrixMaterial, null);
+            var renumbering = new int[]{0,3,2,1,4,7,6,5};
+            for (int i1 = 0; i1 < elementConnectivity.GetLength(0); i1++)
+            {
+                List<Node> nodeSet = new List<Node>();
+                for (int j = 0; j < 8; j++)
+                {
+                    int nodeID = elementConnectivity[i1, j];
+                    nodeSet.Add((Node)model.NodesDictionary[nodeID]);
+                }
+
+                var e1 = new Element()
+                {
+                    ID = i1,
+                    ElementType = new Hexa8NonLinear(matrixMaterial, GaussLegendre3D.GetQuadratureWithOrder(3, 3, 3)) // dixws to e. exoume sfalma enw sto beambuilding oxi//edw kaleitai me ena orisma to Hexa8
+                };
+
+                for (int j = 0; j < 8; j++)
+                {
+                    int nodeID = elementConnectivity[i1, renumbering[j]];
+                    e1.NodesDictionary.Add(nodeID, model.NodesDictionary[nodeID]);
+                }
+                model.ElementsDictionary.Add(e1.ID, e1);
+                model.SubdomainsDictionary[subdomainID].Elements.Add(e1);
+            }
+        }
+
+        private int[,] GetHexaRveConnectivity()
+        {
+            int numberOfElements = hexa1 * hexa2 * hexa3;
+            int[,] elementConnectivity = new int[numberOfElements,8];
+
+            int counterElement = 0;
+            for (int i = 0; i < hexa3; i++)
+            {
+                for (int j = 0; j < hexa2; j++)
+                {
+                    for (int k = 0; k < hexa1; k++)
+                    {
+                        elementConnectivity[counterElement, 0] = k + (hexa1 + 1) * j + (hexa1 + 1) * (hexa2 + 1) * i;
+                        elementConnectivity[counterElement, 1] = elementConnectivity[counterElement, 0] + 1;
+                        elementConnectivity[counterElement, 2] = elementConnectivity[counterElement, 1] + (hexa1 + 1);
+                        elementConnectivity[counterElement, 3] = elementConnectivity[counterElement, 2] - 1;
+
+                        elementConnectivity[counterElement, 4] = k + (hexa1 + 1) * j + (hexa1 + 1) * (hexa2 + 1) * (i+1);
+                        elementConnectivity[counterElement, 5] = elementConnectivity[counterElement, 4] + 1;
+                        elementConnectivity[counterElement, 6] = elementConnectivity[counterElement, 5] + (hexa1 + 1);
+                        elementConnectivity[counterElement, 7] = elementConnectivity[counterElement, 6] - 1;
+                        
+                        counterElement++;
+                    }
+                }
+            }
+
+            return elementConnectivity;
+        }
+
+        private (int[] NodeIds, double[,] node_coords) GetHexaRveNodesData()
+        {
+            int numberOfNodes = (hexa1 + 1) * (hexa2 + 1) * (hexa3 + 1);
+            int[] NodeIds = new int[numberOfNodes];
+            double[,] node_coords = new double[numberOfNodes, 3];
+
+            int counterNode=0;
+
+            for (int  i = 0;  i <= hexa3;  i++)
+            {
+                for (int j = 0; j <= hexa2; j++)
+                {
+                    for (int k = 0; k <= hexa1; k++)
+                    {
+                        NodeIds[counterNode] = counterNode;
+                        node_coords[counterNode, 0] = k * (L01 / hexa1) - L01 * 0.5;
+                        node_coords[counterNode, 1] = j * (L02 / hexa2) - L02 * 0.5;
+                        node_coords[counterNode, 2] = i * (L03 / hexa3) - L03 * 0.5;
+                        counterNode++;
+                    }
+                }
+            }
+
+            return (NodeIds, node_coords);
+        }
+
+        public Dictionary<Node, IList<IDofType>> GetModelRigidBodyNodeConstraints(Model model)
+        {
+            Dictionary<Node, IList<IDofType>> RigidBodyNodeConstraints = new Dictionary<Node, IList<IDofType>>();
+            var rigidNodes = RigidNodes;
+
+            RigidBodyNodeConstraints.Add(model.NodesDictionary[rigidNodes[0]], new List<IDofType>() { StructuralDof.TranslationX, StructuralDof.TranslationY, StructuralDof.TranslationZ });
+            RigidBodyNodeConstraints.Add(model.NodesDictionary[rigidNodes[1]], new List<IDofType>() { StructuralDof.TranslationX, StructuralDof.TranslationY, StructuralDof.TranslationZ });
+            RigidBodyNodeConstraints.Add(model.NodesDictionary[rigidNodes[2]], new List<IDofType>() { StructuralDof.TranslationX, StructuralDof.TranslationY, StructuralDof.TranslationZ });
+
+            return RigidBodyNodeConstraints;
+        }
+
+        private List<int> RigidNodes => new List<int>
+        {
+            { 0},
+            {hexa1 },
+            {(hexa1+1)*hexa2 }
+        };
+
+        public IRVEbuilder Clone(int a)
+        {
+            return new CntReinforcedElasticNanocomposite(this.matrixMaterial, numberOfCnts);
+        }
+    }
+}
--- ISAAR.MSolve.Tests/FEMpartB/LinearRvesCompositeMaterialTet.cs
+++ ISAAR.MSolve.Tests/FEMpartB/LinearRvesCompositeMaterialTet.cs
@@ -4,6 +4,7 @@ using ISAAR.MSolve.LinearAlgebra.Matrices;
 using ISAAR.MSolve.LinearAlgebra.Vectors;
 using ISAAR.MSolve.Materials;
 using ISAAR.MSolve.Materials.Interfaces;
+using ISAAR.MSolve.MSAnalysis.RveTemplatesPaper;
 using ISAAR.MSolve.MultiscaleAnalysis;
 using ISAAR.MSolve.MultiscaleAnalysis.Interfaces;
 using ISAAR.MSolve.Solvers.Direct;
@@ -62,7 +63,7 @@ namespace ISAAR.MSolve.Tests.FEMpartB
         }
 
         [Fact]
-        public static void CheckShellScaleTransitionsAndMicrostructure()
+        public static void CheckShellScaleTransitionsAndMicrostructure()// for mtwin
         {
 
 
@@ -107,6 +108,66 @@ namespace ISAAR.MSolve.Tests.FEMpartB
                                                                             stressesCheck5));
             Assert.True(BondSlipTest.AreDisplacementsSame(Matrix1.CopyToArray2D(), consCheck1));
             Assert.True(BondSlipTest.AreDisplacementsSame(Matrix1.CopyToArray2D(), material4.ConstitutiveMatrix.CopytoArray2D()));
+        }
+
+        [Fact]
+        public static void CheckEmbeddinForSimpleCntRve()// for mtwin 
+        {
+
+
+            double E_disp = 3.5e9; /*Gpa*/ double ni_disp = 0.4; // stather Poisson
+            var outterMaterial = new ElasticMaterial3DtotalStrain() { PoissonRatio = 0.4, YoungModulus = 3.5e9 }; //pascal
+            
+            var Vec1 = Vector.CreateFromArray(new double[3] { 1, 0, 0 });
+            var Vec2 = Vector.CreateFromArray(new double[3] { 0.5, 2, 0 });
+            var strain = new double[3] { 0.01, 0, 0 };
+
+
+            
+
+
+            IdegenerateRVEbuilder homogeneousRveBuilder1 = new CntReinforcedElasticNanocompositeEmbedding(outterMaterial);
+            var material4 = new MicrostructureShell2D(homogeneousRveBuilder1,
+                model => (new SkylineSolver.Builder()).BuildSolver(model), false, 1)
+            {
+                TangentVectorV1 = new double[3] { Vec1[0], Vec1[1], Vec1[2] },
+                TangentVectorV2 = new double[3] { Vec2[0], Vec2[1], Vec2[2] }
+            };
+            var Matrix2 = Matrix.CreateZero(3, 3); for (int i1 = 0; i1 < 3; i1++) { for (int i2 = 0; i2 < 3; i2++) { Matrix2[i1, i2] = material4.ConstitutiveMatrix[i1, i2]; } }
+            material4.UpdateMaterial(strain);
+            double[] stressesCheck4 = new double[3] { material4.Stresses[0], material4.Stresses[1], material4.Stresses[2] };
+            double[,] consCheck2 = new double[3, 3];
+            for (int i1 = 0; i1 < 3; i1++) { for (int i2 = 0; i2 < 3; i2++) { consCheck2[i1, i2] = material4.ConstitutiveMatrix[i1, i2]; } }
+
+            //-------------Check 2 steps savestate etc---------------
+            material4.SaveState();
+            material4.UpdateMaterial(new double[3] { 2 * strain[0], 2 * strain[1], 2 * strain[2] });
+            double[] stressesCheck5 = new double[3] { material4.Stresses[0], material4.Stresses[1], material4.Stresses[2] };
+
+            double[,] expectedMatrix2 = new double[,] { {4703776041.736434, 481770833.3322795, -553385416.66614187 },
+                { 481770833.3322798, 260416666.66737381, -130208333.33368689},
+                {-553385416.66614175, -130208333.33368698, 377604166.66684514 } };
+
+            double[] stressExpected4 = new double[] { 47790111.913443446, 4850263.9596146792, - 5622366.1073777452 };
+
+            double[,] expectedConsCheck2 = new double[3, 3]; 
+            expectedConsCheck2[0, 0]=	4854505478.4989767	;
+		    expectedConsCheck2[0, 1]=	488284142.356189	;
+		    expectedConsCheck2[0, 2]=	-571118291.57952726	;
+		    expectedConsCheck2[1, 0]=	488284142.35618788	;
+		    expectedConsCheck2[1, 1]=	261953606.24573845	;
+		    expectedConsCheck2[1, 2]=	-132674231.51461451	;
+		    expectedConsCheck2[2, 0]=	-571118291.5795269	;
+		    expectedConsCheck2[2, 1]=	-132674231.51461455	;
+            expectedConsCheck2[2, 2] = 386913800.001923;
+
+            double[] stressExpected5 = new double[] { 48802908.700278834, 4908545.9616696825, -5741518.6704506781 };
+
+
+            Assert.True(AreDisplacementsSame(stressExpected4, stressesCheck4));
+            Assert.True(AreDisplacementsSame(stressExpected5, stressesCheck5));
+            Assert.True(BondSlipTest.AreDisplacementsSame(Matrix2.CopyToArray2D(), expectedMatrix2));
+            Assert.True(BondSlipTest.AreDisplacementsSame(consCheck2, expectedConsCheck2));
         }
 
         [Fact]
